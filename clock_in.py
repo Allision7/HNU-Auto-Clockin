d831a965a244970fb7f45fa259cb941f3c89bf30
Allision7 <65100730+Allision7@users.noreply.github.com>
2022-01-13 12:47:41 +0800

    Update clock_in.py

--- clock_in.py
+++ clock_in.py
@@ -1,90 +1,82 @@
-from aip import AipOcr
+import sys
+
 import requests
 import json
-import argparse
 import re
-
-# 初始化变量
-parser = argparse.ArgumentParser()
-parser.add_argument('--username', type=str, default=None)
-parser.add_argument('--password', type=str, default=None)
-parser.add_argument('--province', type=str, default=None)
-parser.add_argument('--city', type=str, default=None)
-parser.add_argument('--county', type=str, default=None)
-parser.add_argument('--app_id', type=str, default=None)
-parser.add_argument('--api_key', type=str, default=None)
-parser.add_argument('--secret_key', type=str, default=None)
-args = parser.parse_args()
-
-# 初始化OCR识别API
-OCRClient = AipOcr(args.app_id, args.api_key, args.secret_key)
 
 def captchaOCR():
     captcha = ''
+    token = ''
     while len(captcha) != 4:
-        token = json.loads(requests.get('https://fangkong.hnu.edu.cn/api/v1/account/getimgvcode').text)['data']['Token']
-        captcha = OCRClient.basicGeneralUrl(f'https://fangkong.hnu.edu.cn/imagevcode?token={token}')['words_result'][0]['words']
+        token = requests.get('https://fangkong.hnu.edu.cn/api/v1/account/getimgvcode').json()['data']['Token']
+        headers={}
+        data={
+            "url":f"https://fangkong.hnu.edu.cn/imagevcode?token={token}",
+            "detect_direction":"false"
+        }
+        headers[b'Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+        response=requests.post("https://aip.baidubce.com/rest/2.0/ocr/v1/webimage?access_token=24.c03a57cc9fb60078a57a08528d8148a9.2592000.1644575167.282335-25504018",headers=headers,data=data)
+        print(response.json())
+        captcha=response.json()['words_result'][0]['words']
     return token, captcha
 
 def login():
     login_url = 'https://fangkong.hnu.edu.cn/api/v1/account/login'
     token, captcha = captchaOCR()
-    login_info = {"Code":args.username,"Password":args.password,"VerCode":captcha,"Token":token}
-    
+    login_info = {"Code": username, "Password": password, "VerCode": captcha, "Token": token}
     set_cookie = requests.post(login_url, json=login_info).headers['set-cookie']
+    print("set_cookie:",set_cookie)
     regex = r"\.ASPXAUTH=(.*?);"
     ASPXAUTH = re.findall(regex, set_cookie)[2]
+    print("ASPXAUTH:",ASPXAUTH)
 
     headers = {'Cookie': f'.ASPXAUTH={ASPXAUTH}; TOKEN={token}'}
     return headers
 
-def setLocation():
-    location = json.loads(requests.get(f'http://api.tianditu.gov.cn/geocoder?ds={{"keyWord":\"{args.province+args.city+args.county}\"}}&tk=2355cd686a32d016021bffbc4a69d880').text)["location"]
-    real_address = "泽林镇福盛花园" # 在此填写详细地址
-    return location["lon"], location["lat"], real_address
-
 def main():
     clockin_url = 'https://fangkong.hnu.edu.cn/api/v1/clockinlog/add'
     headers = login()
-    lon, lat, real_address = setLocation()
-    clockin_data = {"Temperature":"null",
-                    "RealProvince":args.province,
-                    "RealCity":args.city,
-                    "RealCounty":args.county,
-                    "RealAddress":real_address,
-                    "IsUnusual":"0",
-                    "UnusualInfo":"",
-                    "IsTouch":"0",
-                    "IsInsulated":"0",
-                    "IsSuspected":"0",
-                    "IsDiagnosis":"0",
-                    "tripinfolist":[{"aTripDate":"","FromAdr":"","ToAdr":"","Number":"","trippersoninfolist":[]}],
-                    "toucherinfolist":[],
-                    "dailyinfo":{"IsVia":"0","DateTrip":""},
-                    "IsInCampus":"0",
-                    "IsViaHuBei":"0",
-                    "IsViaWuHan":"0",
-                    "InsulatedAddress":"",
-                    "TouchInfo":"",
-                    "IsNormalTemperature":"1",
-                    "Longitude":lon,
-                    "Latitude":lat
+    clockin_data = {"Temperature": "null",
+                    "RealProvince": "湖南省",
+                    "RealCity": "长沙市",
+                    "RealCounty": "岳麓区",
+                    "RealAddress": "宿舍",
+                    "IsUnusual": "0",
+                    "UnusualInfo": "",
+                    "IsTouch": "0",
+                    "IsInsulated": "0",
+                    "IsSuspected": "0",
+                    "IsDiagnosis": "0",
+                    "tripinfolist": [
+                        {"aTripDate": "", "FromAdr": "", "ToAdr": "", "Number": "", "trippersoninfolist": []}],
+                    "toucherinfolist": [],
+                    "dailyinfo": {"IsVia": "0", "DateTrip": ""},
+                    "IsInCampus": "1",
+                    "IsViaHuBei": "0",
+                    "IsViaWuHan": "0",
+                    "InsulatedAddress": "",
+                    "TouchInfo": "",
+                    "IsNormalTemperature": "1",
+                    "Longitude": 112.925896,
+                    "Latitude": 28.238775
                     }
-
     clockin = requests.post(clockin_url, headers=headers, json=clockin_data)
-
+    print(clockin.text)
     if clockin.status_code == 200:
         if '成功' in clockin.text or '已提交' in clockin.text:
             isSucccess = 0
+        else:
+            isSucccess = 1
+            print(json.loads(clockin.text)['msg'])
     else:
         isSucccess = 1
     print(json.loads(clockin.text)['msg'])
-
     return isSucccess
-
-for i in range(10):
-    try:    
-        main()
-        break
-    except:
-        continue
+if __name__ == '__main__':
+    username = "吕斐"
+    password = "哈哈哈"
+    if sys.argv[1]:
+        username = sys.argv[1]
+    if sys.argv[2]:
+        password = sys.argv[2]
+    main()
